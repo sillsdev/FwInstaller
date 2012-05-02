b4af8669d3d528b17feea2bad8d169622ea67f9d
Alistair Imrie <alistair_underscore_imrie_at@sil_dot.org>
2012-05-02 21:42:35 +0100

    Revamped Encoding Converters by moving some files around and deleteing others. Restored speed of localization build on Windows.

--- Wix Installer Build MM FW_EC_40.bat
+++
@@ -1,5 +0,0 @@
-del FW_EC_40.mmp.wxs.log
-del WixLinkFW_EC_40.log
-ProcessWixMMs.js FW_EC_40.mm.wxs
-"%WIX%bin\candle" -nologo FW_EC_40.mmp.wxs >FW_EC_40.mmp.wxs.log
-"%WIX%bin\light" -nologo FW_EC_40.mmp.wixobj -out FW_EC_40.msm >WixLinkFW_EC_40.log
--- Wix Installer Build MM FW_ICU40.bat
+++
@@ -1,5 +0,0 @@
-del FW_ICU40.mmp.wxs.log
-del WixLinkFW_ICU40.log
-ProcessWixMMs.js FW_ICU40.mm.wxs
-"%WIX%bin\candle" -nologo FW_ICU40.mmp.wxs >FW_ICU40.mmp.wxs.log
-"%WIX%bin\light" -nologo FW_ICU40.mmp.wixobj -out FW_ICU40.msm >WixLinkFW_ICU40.log
