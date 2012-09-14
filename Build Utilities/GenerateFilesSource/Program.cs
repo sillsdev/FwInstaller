ec9999bb5329132a4aec0bad402d4353519a5e2f
Alistair Imrie <alistair_underscore_imrie_at@sil_dot.org>
2012-09-14 22:05:19 +0100

    Revamp of installer build to use MSBuild instead of NAnt. Several WIX sources have been edited so that they no longer need preprocessing, typically by replacing parameterized paths with hard-coded Output\Release. GenerateFilesSource has been comprehensively reworked, not only abolishing NAnt calls, but significanlty speeded up.

--- Build Utilities/GenerateFilesSource/Program.cs
+++ Build Utilities/GenerateFilesSource/Program.cs
@@ -1,34 +1,50 @@
-﻿namespace GenerateFilesSource
+﻿using System.Diagnostics;
+
+namespace GenerateFilesSource
 {
 	class Program
 	{
 
 		static void Main(string[] args)
 		{
-			// Set default build type:
-			string buildType = "Release";
-			// Flag to determine if we will reuse last time's output folders for FLEx and TE:
-			bool reuseOutput = false;
 			// Flag to indicate if we should write a report about every decision we make:
-			bool report = false;
+			var report = false;
+			// Flag to indicate if we should launch the installer integrity tester at the end:
+			var testIntegrity = false;
+			// Flag to indicate if we should include all feature-unassigned files in FW_Core:
+			var addOrphans = false;
 
 			foreach (var arg in args)
 			{
 				switch (arg.ToLowerInvariant())
 				{
-					case "debug":
-						buildType = "Debug";
-						break;
-					case "reuse":
-						reuseOutput = true;
-						break;
 					case "report":
 						report = true;
 						break;
+					case "check":
+						testIntegrity = true;
+						break;
+					case "addorphans":
+						addOrphans = true;
+						break;
 				}
 			}
-			Generator gen = new Generator(buildType, reuseOutput, report);
+			var gen = new Generator(report, addOrphans);
 			gen.Run();
+
+			if (testIntegrity)
+			{
+				// Run TestInstallerIntegrity.exe:
+				var procIntegrityTester = new Process
+				{
+					StartInfo =
+					{
+						FileName = "TestInstallerIntegrity.exe",
+					}
+				};
+				procIntegrityTester.Start();
+				procIntegrityTester.WaitForExit();
+			}
 		}
 	}
 }
